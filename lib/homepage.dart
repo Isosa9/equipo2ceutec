d4bf7c2cb71f74312fb08573e94a8586e0693830
Isosa9 <sosayorleni72@gmail.com>
2024-12-06 10:56:44 -0600

    Home page

---
+++ lib/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        color: Color(0xFF2D2F41),
+      ),
+    );
+  }
+}
