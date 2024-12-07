c6f5a94d35806f3711fafa3786c8c01a508dd2f8
Alejandro-ZG <ale.xavala1980@gmail.com>
2024-12-06 20:45:17 -0600

    automatizacion de calculos

--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -1,4 +1,5 @@
 
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,13 +10,24 @@ class ClockView extends StatefulWidget {
 }
 
 class _ClockViewState extends State<ClockView> {
+  @override void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer){
+      setState(() {
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 300,
       height: 300,
-      child: CustomPaint(
-        painter: ClockPainter(),
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
       ),
     );
   }
