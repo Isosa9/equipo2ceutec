9d09651fef2cfb899161905dd3b3dee4bb3cf007
Isosa9 <sosayorleni72@gmail.com>
2024-12-07 09:24:03 -0600

    Cambios de hora y dia

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,42 +10,77 @@ class Homepage extends StatefulWidget {
 class _HomePageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d, mm').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split('-').first;
+    var offsetSign = '';
+
+    if (!timezoneString.startsWith('-')) offsetSign = '+';
+    print(timezoneString);
+
     return Scaffold(
       backgroundColor: Color(0xFF2DF41),
-      body: Container(
-        padding: EdgeInsets.all(32),
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Clock',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            SizedBox(height: 32),
-            Text(
-              'Clock',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            Text(
-              'Clock',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            ClockView(),
-            Text(
-              'Tinezone',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            Row(
+      body: Row(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {},
+                child: Column(
+                  children: <Widget>[
+                    FlutterLogo(),
+                    Text(
+                      'Clock',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(32),
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Icon(
-                  Icons.language,
-                  color: Colors.white,
+                Text(
+                  'Clock',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                SizedBox(height: 32),
+                Text(
+                  'Clock',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                Text(
+                  formattedDate,
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                ClockView(),
+                Text(
+                  'Tinezone',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.language,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'UTC' + offsetSign + timezoneString,
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ],
                 )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
